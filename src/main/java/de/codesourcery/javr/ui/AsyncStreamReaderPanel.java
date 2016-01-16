054797cc6fe2613bf1c2f81c2e5a116398c05bf0
Tobias Gierke <tobias.gierke@code-sourcery.de>
2016-01-16 16:41:26 +0100

    added nice uC upload window that shows stdout/stderr in realtime and supports cancelling the upload process

---
+++ src/main/java/de/codesourcery/javr/ui/AsyncStreamReaderPanel.java
@@ -0,0 +1,234 @@
+package de.codesourcery.javr.ui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+public class AsyncStreamReaderPanel extends JPanel 
+{
+    private static final Logger LOG = Logger.getLogger(AsyncStreamReaderPanel.class);
+
+    private static final AtomicInteger ID = new AtomicInteger();
+    
+    private final JTextArea textArea = new JTextArea();
+
+    private final Object THREAD_LOCK = new Object();
+    
+    private volatile boolean printTimestamps=true;
+    
+    // @GuardedBy( THREAD_LOCK )    
+    private boolean threadEnabled = false;
+    // @GuardedBy( THREAD_LOCK )
+    private Reader streamReader = null;
+    // @GuardedBy( THREAD_LOCK )
+    private InputStream stream = null;
+    
+    private volatile boolean terminate = false;
+    
+    private volatile boolean stopOutput = false;
+
+    private volatile int maxBufferSize = 1024;
+    
+    private final Thread ioThread = new Thread("io-thread-"+ID.incrementAndGet()) { 
+
+        private void closeReader() 
+        {
+            IOUtils.closeQuietly( streamReader );
+            streamReader = null;
+            stream = null;
+            threadEnabled = false;
+        }
+        
+        public void run() 
+        {
+            final char[] buffer = new char[20];           
+            LOG.debug("Thread started.");
+            while( ! terminate  ) 
+            {
+                synchronized(THREAD_LOCK) 
+                {
+                    if ( ! threadEnabled || streamReader == null ) 
+                    {
+                        try 
+                        {
+                            if ( LOG.isTraceEnabled() ) {
+                                LOG.trace("Thread going to sleep (disabled, got IO stream: "+(streamReader != null)+")");
+                            }
+                            THREAD_LOCK.wait( 200 );
+                        } catch (InterruptedException e) { /* loop */ }
+                        continue;
+                    } 
+                    try 
+                    {
+                        if ( ! streamReader.ready() && stream.available() <= 0 ) 
+                        {
+                            continue;
+                        }
+                        
+                        if ( LOG.isTraceEnabled() ) {
+                            LOG.trace("Reading from buffer...");
+                        } 
+                        stopOutput = false;
+                        while ( streamReader.ready() && ! stopOutput ) 
+                        {
+                            if ( LOG.isTraceEnabled() ) {
+                                LOG.trace("Reading from buffer...");
+                            }                            
+                            final int len = streamReader.read( buffer );
+                            if ( LOG.isTraceEnabled() ) {
+                                LOG.trace("Got "+len+" characters");
+                            }  
+                            if ( len > 0 ) 
+                            {
+                                appendText( new String(buffer,0,len) );
+                            } 
+                            else 
+                            {
+                                appendText("\nEnd of input reached.");
+                                LOG.debug("EOF reached");
+                                closeReader();
+                                break;
+                            }
+                        }
+                    } 
+                    catch(IOException e) 
+                    {
+                        if ( ! "stream closed".equalsIgnoreCase( e.getMessage() ) ) {
+                            appendText("\nCAUGHT ERROR: "+e.getMessage());
+                            if ( LOG.isDebugEnabled() ) {
+                                LOG.info("run(): Caught "+e.getMessage(),e);
+                            } else {
+                                LOG.info("run(): Caught "+e.getMessage() );
+                            }
+                        } else {
+                            LOG.debug("stream closed");
+                        }
+                        closeReader();
+                    } 
+                }
+            }
+            closeReader();
+            LOG.debug("Thread terminated.");
+        }
+    };
+    
+    public AsyncStreamReaderPanel() 
+    {
+        textArea.setEditable( false );
+        textArea.setColumns( 25 );
+        setLayout( new GridBagLayout() );
+        final GridBagConstraints cnstrs = new GridBagConstraints();
+        cnstrs.weightx = 1; cnstrs.weighty = 1;
+        cnstrs.fill = GridBagConstraints.BOTH;
+        add( new JScrollPane( textArea ) , cnstrs );
+
+        addAncestorListener( new AncestorListener() {
+            
+            @Override
+            public void ancestorRemoved(AncestorEvent event) 
+            {
+                synchronized (THREAD_LOCK) 
+                {
+                    threadEnabled = false;
+                }
+            }
+            
+            @Override
+            public void ancestorMoved(AncestorEvent event) { }
+            
+            @Override
+            public void ancestorAdded(AncestorEvent event) {
+                synchronized (THREAD_LOCK) 
+                {
+                    threadEnabled = true;
+                    THREAD_LOCK.notifyAll();
+                }
+            }
+        });
+
+        ioThread.setDaemon( true );
+        ioThread.start();
+    }    
+
+    public void setInputStream(final InputStream stream) 
+    {
+        SwingUtilities.invokeLater( () -> 
+        {
+            stopOutput = true;
+            synchronized (THREAD_LOCK) 
+            {
+                if ( streamReader != null ) {
+                    IOUtils.closeQuietly( streamReader );
+                }
+                threadEnabled = stream != null ;
+                this.stream = stream;
+                streamReader = stream != null ? new InputStreamReader(stream) : null;
+            }
+        });
+    }
+    
+    public void appendText(String msg) 
+    {
+        if ( printTimestamps && msg.contains("\n" ) ) 
+        {
+            final DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            final String date = df.format( ZonedDateTime.now() );
+            msg = msg.replace("\n" , "\n"+date+": ");
+        }
+        
+        final String finalMsg = msg;
+        final Runnable runnable = () -> 
+        {
+            String newString = textArea.getText() + finalMsg;
+            final int maxSize = maxBufferSize;
+            if ( newString.length() > maxSize ) 
+            {
+                newString = newString.substring( newString.length() - maxSize , newString.length() );
+            }
+            textArea.setText( newString );  
+        };
+        if ( SwingUtilities.isEventDispatchThread() ) 
+        {
+            runnable.run();
+        } else {
+            SwingUtilities.invokeLater( runnable);
+        }
+    }
+
+    public void setMaxBufferSize(int maxBufferSize) 
+    {
+        if ( maxBufferSize < 1 ) {
+            throw new IllegalArgumentException("Buffer too small");
+        }
+        this.maxBufferSize = maxBufferSize;
+    }
+    
+    public void terminate() 
+    {
+        terminate = true;
+    }
+
+    public void setPrintTimestamps(boolean printTimestamps) {
+        this.printTimestamps = printTimestamps;
+    }
+    
+    public boolean isPrintTimestamps() {
+        return printTimestamps;
+    }
+}
---
+++ src/main/java/de/codesourcery/javr/ui/ProcessWindow.java
@@ -0,0 +1,167 @@
+package de.codesourcery.javr.ui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+import org.apache.log4j.Logger;
+
+public class ProcessWindow extends JDialog 
+{
+    private static final Logger LOG = Logger.getLogger(ProcessWindow.class);
+    
+    private final AsyncStreamReaderPanel stdOutPanel = new AsyncStreamReaderPanel();
+    private final AsyncStreamReaderPanel stdErrPanel = new AsyncStreamReaderPanel();
+    
+    private volatile Process process;
+    private volatile boolean terminatedByUser;
+    
+    private final JButton cancel = new JButton("Cancel");
+    
+    public ProcessWindow(String title,String msg,boolean modal) 
+    {
+        super((JFrame) null,title,modal);
+        
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        addWindowListener( new WindowAdapter() 
+        {
+            @Override
+            public void windowClosed(WindowEvent e) 
+            {
+                terminatedByUser = true;
+                stop();
+            }
+        });
+        
+        getContentPane().setLayout( new GridBagLayout() );
+        
+        int y = 0;
+        GridBagConstraints cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0.3;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.insets = new Insets( 10 , 0 , 10 , 0 );
+        cnstrs.fill = GridBagConstraints.BOTH;
+        final JTextArea msgArea = new JTextArea( msg );
+        msgArea.setEditable( false );
+        msgArea.setRows( 5 );
+        msgArea.setWrapStyleWord( true );
+        msgArea.setLineWrap(true);
+        getContentPane().add( new JScrollPane(msgArea) , cnstrs );
+        
+        cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.fill = GridBagConstraints.HORIZONTAL;
+        getContentPane().add( new JLabel("Standard output:") , cnstrs );
+        
+        cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0.35;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.fill = GridBagConstraints.BOTH;        
+        getContentPane().add( stdOutPanel , cnstrs );
+        
+        cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.fill = GridBagConstraints.HORIZONTAL;
+        getContentPane().add( new JLabel("Standard error:") , cnstrs );
+        
+        cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0.35;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.fill = GridBagConstraints.BOTH;        
+        getContentPane().add( stdErrPanel , cnstrs );
+        
+        cnstrs = new GridBagConstraints();
+        cnstrs.weightx=1; cnstrs.weighty = 0;
+        cnstrs.gridx = 0 ; cnstrs.gridy = y++;
+        cnstrs.fill = GridBagConstraints.NONE;  
+        cnstrs.insets = new Insets( 10 , 0 , 10 , 0 );
+        cancel.addActionListener( ev -> 
+        {
+            if ( terminatedByUser ) {
+                dispose();
+            } else {
+                terminatedByUser = true;
+                cancel.setText("Close");
+                stop();
+            }
+        });
+        getContentPane().add( cancel , cnstrs );
+        
+        setPreferredSize( new Dimension(400,400 ) );
+    }
+    
+    private void stop() 
+    {
+        if ( process != null ) 
+        {
+            process.destroyForcibly();
+            process = null;
+        }
+        stdOutPanel.terminate();
+        stdErrPanel.terminate();        
+    }
+    
+    public void execute(final List<String> command) 
+    {
+        final Thread t = new Thread() 
+        {
+            public void run() 
+            {
+                try 
+                {
+                    SwingUtilities.invokeLater( () -> 
+                    {
+                        pack();
+                        setLocationRelativeTo( null );
+                        setVisible( true );
+                    });
+                    
+                    LOG.debug("Starting process "+command.stream().collect(Collectors.joining(" ") ) );
+                    final ProcessBuilder builder = new ProcessBuilder( command );                    
+                    final Process process = builder.start();
+                    final InputStream stdOut = process.getInputStream();
+                    final InputStream stdErr = process.getErrorStream();
+                    stdOutPanel.setInputStream( stdOut );
+                    stdErrPanel.setInputStream( stdErr );
+                    final int exitCode = process.waitFor();
+                    LOG.debug("Process "+command+" exited with "+exitCode+" (terminated by user: "+terminatedByUser+")");
+                    if ( terminatedByUser ) {
+                        stdOutPanel.appendText("\nProcess terminated by user.");
+                    } else if ( exitCode != 0 ) {
+                        stdErrPanel.appendText("\nProcess failed with exit code "+exitCode);
+                    } else {
+                        stdOutPanel.appendText("\nProcess finished successfully.");
+                        terminatedByUser = true;
+                        SwingUtilities.invokeLater( () -> cancel.setText("Close") );
+                    }
+                    Thread.sleep(500);
+                    Thread.yield();
+                    process.destroy();
+                } 
+                catch (Exception e) 
+                {
+                    throw new RuntimeException(e);
+                }                   
+            }
+        };
+        t.setDaemon( true );
+        t.start();
+    }
+}
