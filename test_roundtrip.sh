504dd82b0dba28ea5fdbd74d936d95551e8017b0
Tobias Gierke <tobias.gierke@voipfuture.com>
2017-05-12 19:41:59 +0200

    added script for doing round-trip comparisons against avr-as (random data -> disassemble -> compile -> compare against input binary)

---
+++ test_roundtrip.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export QUIET="1"
+
+# create random data
+dd if=/dev/urandom of=random bs=1k count=16
+
+# disassemble
+./disassemble.sh random
+if [ "$?" != "0" ] ; then
+        echo "ERROR: Disassembler failed"
+        exit 1
+fi
+
+# JAVR assemble
+./javr_assemble.sh --hide-warnings random.javr.asm
+if [ "$?" != "0" ] ; then
+        echo "ERROR: javr failed."
+        exit 1
+fi 
+
+# avr-as assemble
+./avr_assemble.sh random.avr-as.asm
+if [ "$?" != "0" ] ; then
+	echo "ERROR: avr-as failed."
+        exit 1
+fi	
+
+# compare binary images
+MYSUM=`md5sum random.javr.asm.flash.raw | cut -d" " -f1`
+GNUSUM=`md5sum random.avr-as.asm.raw | cut -d" " -f1` 
+
+if [ "$MYSUM" != "$GNUSUM" ] ; then
+	echo "ERROR: Checksums differ:"
+	echo "ERROR: random.javr.asm.flash.raw => $MYSUM" 
+	echo "ERROR: random.avr-as.asm.raw     => $GNUSUM"
+	exit 1
+else
+	echo "Test passed."
+fi
