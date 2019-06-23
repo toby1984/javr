92f136ff5e15b8a1658b5b351823825500166721
Tobias Gierke <tobias.gierke@code-sourcery.de>
2019-06-23 21:34:09 +0200

    added assembler routines for SPI communication with ENC28J60 ethernet shield

--- examples/projects/dcf77/src/main.asm
+++ examples/projects/dcf77/src/main.asm
@@ -27,9 +27,16 @@ jmp onirq ; Analog comparator
 jmp onirq ; 2-wire interface I2C
 jmp onirq ; Store program memory ready
 
+.equ CPU_FREQ = 8000000 ; 8 Mhz
+
 ; SPI slave-select output pins on PortB to
-.equ SPI_SS_ETHERNET = 1<<PB0
-.equ SPI_SS_DCF77 = 1<<PB1
+.equ SPI_SS_ETHERNET_BIT = 0
+.equ SPI_SS_DCF77_BIT = 1
+.equ SPI_SS_ETHERNET = 1<<SPI_SS_ETHERNET_BIT
+.equ SPI_SS_DCF77 = 1<<SPI_SS_DCF77_BIT
+
+; Number of cycles to wait before assuming no more data will be sent
+.equ SPI_READ_TIMEOUT_CYCLES = 1000
 
 ; scratch registers
 .def scratch0 = r17
@@ -88,20 +95,128 @@ spi_init:
   out  SPCR,scratch0
   ret
 
+; =======
+; ECN28J60 SPI transmit 1 byte, read nothing
+; INPUT: r0 - byte to send
+; SCRATCHED: scratch0, r0
+; =======
+spi_transmit_eth_1w0r: ; 1 byte written, zero bytes read
+  cbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select low
+  call spi_tx_delay
+  mov r16, r0 ; r16 = r0
+  call spi_transmit
+  sbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select hi
+  ret
+
+; =======
+; ECN28J60 SPI transmit 1 byte, read 1 byte
+; INPUT: r0 - byte to send
+; RESULT: r0 - received byte
+;         r1 - 0 -> success, 0xff = timeout during read
+; SCRATCHED: scratch0, r0
+; =======
+spi_transmit_eth_1w1r: ; 1 byte written, 1 byte read
+  cbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select low
+  call spi_tx_delay
+  mov r16, r0
+  call spi_transmit
+  call spi_receive
+  sbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select hi
+  ret
+
+; =======
+; ECN28J60 SPI transmit 1 byte, read 2 bytes
+; INPUT: r0 - byte to send
+; RESULT: r0 - second received byte
+;         r1 - 0 -> success, 0xff = timeout during read
+;         r2 - first received byte
+; SCRATCHED: scratch0, r0,r1,r2
+; =======
+spi_transmit_eth_1w2r: ; 1 byte written, 2 bytes read
+  cbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select low
+  call spi_tx_delay
+  mov r16, r0
+  call spi_transmit
+  call spi_receive
+  tst r1
+  brne error
+  mov r2,r0
+  call spi_receive
+.error
+  sbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select hi  
+  ret
+
+; =======
+; Writes up to 255 bytes from SRAM to ECN28J60 buffer
+; INPUT:
+; r0 - number of bytes to write
+; r31:r30 (Z) - Pointer to SRAM
+; SCRATCHED: r0,scratch0
+; =======
+spi_transmit_eth_write_buffer:
+  cbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select low
+  call spi_tx_delay
+.tx_loop
+  LD r16,Z+
+  call spi_transmit
+  dec r0
+  brne tx_loop
+  sbi PORTB, SPI_SS_ETHERNET_BIT ; slave-select hi  
+  ret
+
+; ========
+; Receives one byte via SPI
+; RESULT: r0 - byte that was read
+;         r1 - 0 -> success, 0xff = timeout during read
+; SCRATCHED: scratch0,scratch 1
+; ========
+spi_receive:
+   ldi scratch0, LOW(SPI_READ_TIMEOUT_CYCLES)
+   ldi scratch1, HIGH(SPI_READ_TIMEOUT_CYCLES)
+   clr r1 ; assume success
+.loop
+  ; wait for SPIF flag to be set
+  in r0,SPSR
+  sbrc r0, SPIF
+  rjmp leave ; -> SPIF set
+ 
+  dec scratch0
+  brne skip1
+  dec scratch1
+  brne skip2
+  com r1 ; timeout error
+  ret
+.skip2
+  ldi scratch0, 0xff
+.skip1
+  rjmp loop
+.leave
+; Read received data and return
+; the SPIF bit is cleared by first reading the
+; SPI Status Register with SPIF set, then accessing the SPI Data Register (SPDR).
+  in r0,SPDR
+  ret
+
 ; =========
 ; SPI send byte
 ; INPUT: r16 - data to send
 ; SCRATCHED: scratch0
+; =========
 spi_transmit:
-; TODO: Pull the corresponding slave-select PIN low
-; TODO: Time to wait until transmission starts ?
-; Start transmission of data (r16)
   out  SPDR,r16
-; Wait for transmission complete
+; wait for completion
 .wait
   in  scratch0, SPSR
   sbrs scratch0, SPIF
   rjmp wait
-; TODO: Pull the corresponding slave-select PIN HIGH
+  in scratch0, SPDR ; read data register to clear SPIF flag
   ret
-  
+
+; ====
+; Used to delay actual transmission
+; after slave-select goes low
+; Spec demands at least 120ns delay
+; ====
+spi_tx_delay:
+  nop
+  ret
